dff10a26819d7f6445e32b0c6a0cda09992b8e3d
gogaetf <gorana.bosic@gmail.com>
2024-12-23 20:56:23 +0100

    week 4 0 flink - assignment

---
+++ bootcamp/materials/4-apache-flink-training/sql/select_hosts_avg.sql
@@ -0,0 +1,21 @@
+-- select average number of hits per host
+WITH avg_per_user AS
+(
+	SELECT
+		ip, 
+	    host,
+	    AVG(num_hits) AS avg_num_hits
+	FROM processed_events_session_aggregated
+	GROUP BY 
+		ip, 
+	    host
+	ORDER BY avg_num_hits
+)
+SELECT
+    host,
+    AVG(avg_num_hits) AS avg_num_hits
+FROM avg_per_user
+GROUP BY 
+    host
+ORDER BY avg_num_hits;
+
