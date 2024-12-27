a65f6939a4a3ec3c23ab359962ebeb5d9faac5a7
gogaetf <gorana.bosic@gmail.com>
2024-12-27 19:02:48 +0100

    week 4 - applying analytical patterns - lab 2

---
+++ bootcamp/materials/4-applying-analytical-patterns/lecture-lab/my-work/query_funnel_analysis.sql
@@ -0,0 +1,63 @@
+-- what percentage of users that got to the signup page actually signed up?
+-- will be done without window functions, but with a self join
+
+--select distinct url from events where url like '%user%' order by url;
+
+-- there is a difference in counting conversion rates
+-- option 1: conversion of users - will give optimistic numbers
+-- option 2 conversion of hits - will give ore realistic numbers
+WITH deduped_events AS (
+    SELECT
+        user_id,
+        url,
+        event_time,
+        date(event_time) AS event_date
+    FROM events
+    WHERE
+        user_id IS NOT NULL
+    --    AND url in ('/signup', '/api/v1/user')
+    --    AND url in ('/signup', '/api/v1/login')
+    GROUP BY
+        user_id,
+        url,
+        event_time,
+        date(event_time)
+),
+    selfjoined AS(
+        SELECT
+            d1.user_id,
+            d1.url,
+            d2.url AS destination_url,
+            d1.event_time,
+            date(d1.event_time) AS event_date_d1,
+            d2.event_time AS event_time_destination,
+            date(d2.event_time) AS event_date_destination
+        FROM deduped_events d1
+        JOIN deduped_events d2
+            ON d1.user_id = d2.user_id
+            AND d1.event_date = d2.event_date
+            AND d1.event_time < d2.event_time
+        -- WHERE d1.url = '/signup'
+            -- AND d2.url = '/api/v1/login'
+    ),
+    userlevel AS (
+        SELECT
+            user_id,
+            url,
+            COUNT(1) AS num_hits,
+            SUM(CASE WHEN destination_url = '/api/v1/login' THEN 1 ELSE 0 END) AS converted
+        FROM selfjoined
+        GROUP BY
+            user_id,
+            url
+    )
+SELECT
+    url,
+    SUM(num_hits) as num_hits,
+    SUM(converted) AS num_converted,
+    CAST(SUM(converted) AS REAL) / SUM(num_hits) AS pct_converted
+FROM userlevel
+GROUP BY
+    url
+HAVING SUM(num_hits) > 500
+ORDER BY CAST(SUM(converted) AS REAL) / SUM(num_hits);
---
+++ bootcamp/materials/4-applying-analytical-patterns/lecture-lab/my-work/query_funnel_analysis_rollup.sql
@@ -0,0 +1,31 @@
+WITH events_augmented AS (
+    SELECT
+        COALESCE(d.os_type, 'Unknown') AS os_type,
+        COALESCE(d.device_type, 'Unknown') AS device_type,
+        COALESCE(d.browser_type, 'Unknown') AS browser_type,
+        url,
+        user_id
+    FROM events e
+    JOIN devices d
+        ON e.device_id = d.device_id
+)
+SELECT
+    GROUPING(os_type) AS os_type_grouping,
+    GROUPING(device_type) AS device_type_grouping,
+    GROUPING(browser_type) AS browser_type_grouping,
+    CASE
+        WHEN GROUPING(os_type) = 0
+            AND GROUPING(device_type) = 0
+            AND GROUPING(browser_type) = 0
+        THEN 'os_type__device_type__browser_type'
+        WHEN GROUPING(os_type) = 0 THEN 'os_type'
+        WHEN GROUPING(device_type) = 0 THEN 'device_type'
+        WHEN GROUPING(browser_type) = 0 THEN 'browser_type'
+    END AS aggregation_level,
+    COALESCE(os_type, '(overall)') AS os_type,
+    COALESCE(device_type, '(overall)') AS device_type,
+    COALESCE(browser_type, '(overall)') AS browser_type,
+    COUNT(1)
+FROM events_augmented
+GROUP BY ROLLUP(browser_type, device_type, os_type)
+ORDER BY COUNT(1) DESC;
